56061affc6ca99ff43e764f4c57644999c8ae180
moszap <abc@abc.com>
2022-05-02 14:40:32 +0800

    event v0.2b

--- src/main/java/com/example/sagasample/saga2/SagaManager2.java
+++ src/main/java/com/example/sagasample/saga2/SagaManager2.java
@@ -1,13 +1,17 @@
 package com.example.sagasample.saga2;
 
+import com.example.sagasample.saga1.SagaCommand1;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
+import java.util.Stack;
 
 
 @Slf4j
 public class SagaManager2 {
+
+    Stack<SagaCommand1> undoCommandStack=new Stack<SagaCommand1>();
 
     private static SagaManager2 instance;
 
